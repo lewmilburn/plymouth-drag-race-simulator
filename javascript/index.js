0b564136b79ea05af584a8f3d8cc383d930a812b
lewmilburn <lewiscmilburn@gmail.com>
2022-03-14 18:16:10 +0000

    Added version info

--- javascript/index.js
+++ javascript/index.js
@@ -54,6 +54,7 @@ let makeoverCounter = false;
 let snatchCounter = false;
 let lastChallenge = '';
 let audio = new Audio('audio/song/1.mp3');
+document.getElementById("rev").innerHTML = "Rev. 91";
 function miniChallenge() {
     let miniChallengeScreen = new Scene();
     miniChallengeScreen.clean();
