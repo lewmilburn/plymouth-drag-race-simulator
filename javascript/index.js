5dfa0c7a5e0f00eba0d206df04ebbac04fb10dc6
lewmilburn <lewiscmilburn@gmail.com>
2022-03-14 23:12:10 +0000

    Changed background image.

--- javascript/index.js
+++ javascript/index.js
@@ -55,7 +55,7 @@ let snatchCounter = false;
 let lastChallenge = '';
 let audio = new Audio('audio/song/4.mp3');
 //audio.play();
-document.getElementById("softwareinfo").innerHTML = "&nbsp;&nbsp;Version: build-127.";
+document.getElementById("softwareinfo").innerHTML = "&nbsp;&nbsp;Version: build-128.";
 function miniChallenge() {
     let miniChallengeScreen = new Scene();
     miniChallengeScreen.clean();
