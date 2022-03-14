978102ae5fa4050e7facef206d1656667909fe00
lewmilburn <lewiscmilburn@gmail.com>
2022-03-14 22:09:16 +0000

    Added celebrities edition

--- javascript/index.js
+++ javascript/index.js
@@ -55,7 +55,7 @@ let snatchCounter = false;
 let lastChallenge = '';
 let audio = new Audio('audio/song/4.mp3');
 //audio.play();
-document.getElementById("softwareinfo").innerHTML = "&nbsp;&nbsp;Version: build-114.";
+document.getElementById("softwareinfo").innerHTML = "&nbsp;&nbsp;Version: build-115.";
 function miniChallenge() {
     let miniChallengeScreen = new Scene();
     miniChallengeScreen.clean();
